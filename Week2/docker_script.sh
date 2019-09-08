d00c48df5efde538ecfdb64b29796ac4472d9c46
srikars2001 <55019452+srikars2001@users.noreply.github.com>
2019-09-08 17:44:25 +0530

    Update docker_script.sh

--- Week2/docker_script.sh
+++ Week2/docker_script.sh
@@ -1,3 +1,16 @@
+##edited##
+
+# (common errors)
+
+  #  image source will not be copied in terminal text editor  if the link is too long
+  # image problem solution : visit https://hmp.me/ upload the picture and get short url and paste in html file
+
+
+# whole process is as shown below
+
+
+# docker login
+
 #Pulling NGINX image
 docker pull nginx
 
@@ -26,9 +39,42 @@ cd usr/share/nginx/html/
 #view the default index.html - this is displayed in your browser once NGINX is succesfully up and running
 cat index.html
 
-#exit the container
-exit
+#open browser type
+ 0.0.0.0
+ 
+#default html page is displayed
 
+# log into a container
+docker exec -it nginx1 bash
+
+#change to correct path
+cd usr/share/nginx/html/
+
+# overwrite index.html file
+cat > index.html
+
+#paste your html code
+
+#exit 
+ctrl+D
+
+#open browser type (refresh)
+ 0.0.0.0
+ 
+ #tag your nginx file
+ docker tag <image ID> <dockerhub username>/<image>:<tag>
+ 
+ example:
+ 
+ docker tag 5a3221f0137b srikars2001/nginx:group8
+ 
+ #push image to docker hub
+ docker push your_username/image_name:version
+ 
+example:
+          docker push srikars2001/nginx:group8
+     
+ #  voila assignment completed!!!!!
 
 
 
